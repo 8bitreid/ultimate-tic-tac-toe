f86066dc23dbb9230f709c3bd96226e3cbed84a6
Reid Mewborne <8bitreid@users.noreply.github.com>
2018-10-06 11:17:36 -0500

    Ui update (#14)
    * fixed local run issues
    
    * nav changes, added about page
    
    * adding about page

--- site/ClientApp/app/components/navmenu/navmenu.component.ts
+++ site/ClientApp/app/components/navmenu/navmenu.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'nav-menu',
-    templateUrl: './navmenu.component.html',
-    styleUrls: ['./navmenu.component.css']
+    templateUrl: './navmenu.component.html'
 })
 export class NavMenuComponent {
 }
