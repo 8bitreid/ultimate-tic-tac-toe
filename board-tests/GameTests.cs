f981c7763b378b18e1ae9503639bb26056cf15a5
Dan Fields <daniel.laurent.fields@gmail.com>
2018-10-07 23:50:51 -0400

    Added some unit tests for move validation and player alternation (#17)

---
+++ board-tests/GameTests.cs
@@ -0,0 +1,39 @@
+﻿using game_logic.models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace board_tests
+{
+    public class GameTests
+    {
+        [Fact]
+        public void PlayerOneHasFirstTurn()
+        {
+            var player1 = new Player(1);
+            var player2 = new Player(2);
+
+            var game = new Game(player1, player2);
+          
+            Assert.Equal(player1, game.CurrentPlayer);
+        }
+
+        [Fact]
+        public void PlayersAlternateTurns()
+        {
+            var player1 = new Player(1);
+            var player2 = new Player(2);
+
+            var game = new Game(player1, player2);
+
+            game.Move(player: player1, boardIndex: 1, boardPosition: 1);
+
+            Assert.Equal(player2, game.CurrentPlayer);
+
+            game.Move(player: player2, boardIndex: 1, boardPosition: 2);
+
+            Assert.Equal(player1, game.CurrentPlayer);
+        }
+    }
+}
---
+++ game-logic/models/MoveResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace game_logic.models
+{
+    public class MoveResult
+    {
+        public bool IsValid { get; }
+
+        public MoveResult(bool isValid) => IsValid = isValid;
+    }
+}
